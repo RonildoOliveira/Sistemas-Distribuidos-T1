7cd51ce423b83d59a4945eeb70e43a703ad3eee0
Dieinison <di.jack9@gmail.com>
2016-10-17 23:50:27 -0300

    Chat entre dois processos

---
+++ src/ufc/cc/sd/q03/ChatTCPClient.java
@@ -0,0 +1,79 @@
+package ufc.cc.sd.q03;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ChatTCPClient {
+
+	public static void main(String[] args)
+	{
+		try {
+			Socket sock = new Socket("localhost",6666); //criando socket local
+			// Criando duas threads, uma pra envio e outra pra receber
+			ThreadEnvio threadEnvio = new ThreadEnvio(sock);
+			Thread thread = new Thread(threadEnvio);
+			thread.start();
+			
+			ThreadRecebe recieveThread = new ThreadRecebe(sock);
+			Thread thread2 =new Thread(recieveThread);
+			thread2.start();
+		} catch (Exception e) {System.out.println(e.getMessage());} 
+	}
+}
+class ThreadRecebe implements Runnable{
+	//Instanciando
+	Socket socket = null;
+	BufferedReader receber = null;
+	
+	//construtor
+	public ThreadRecebe(Socket sock) {
+		this.socket = sock;
+	}
+	
+	//método da classe runnable
+	public void run() {
+		try{
+		receber = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+		String msgRecebida = null;
+			while((msgRecebida = receber.readLine())!= null){
+				System.out.println("do Servidor: " + msgRecebida);
+			}
+		}catch(Exception e){System.out.println(e.getMessage());
+	}
+	}
+}
+
+class ThreadEnvio implements Runnable{
+	Socket socket = null;
+	PrintWriter print = null;
+	BufferedReader brinput = null;
+	
+	public ThreadEnvio(Socket sock){
+		this.socket = sock;
+	}
+	
+	//método de runnable
+	public void run(){
+		try{
+			if(socket.isConnected())
+			{
+				System.out.println("Cliente conectado a " + socket.getInetAddress());
+				this.print = new PrintWriter(socket.getOutputStream(), true);	
+			
+			while(true){
+				
+				brinput = new BufferedReader(new InputStreamReader(System.in));
+				String msgServidor = null;
+				msgServidor = brinput.readLine();
+				this.print.println(msgServidor);
+				this.print.flush(); // limpar o printWriter
+			
+				if(msgServidor.equals("SAIR"))
+				break;
+			}
+		socket.close();}}catch(Exception e){System.out.println(e.getMessage());}
+	}
+}
+
---
+++ src/ufc/cc/sd/q03/ChatTCPServidor.java
@@ -0,0 +1,91 @@
+package ufc.cc.sd.q03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ChatTCPServidor {
+	
+	public static void main(String[] args) throws IOException {
+		int port = 6666;
+		System.out.println("Esperando conexões na porta - " + port);
+		
+		ServerSocket ss = new ServerSocket(port); //Instanciando Socket
+		Socket clienteSocket = ss.accept(); //Socket pra aceitar conexões
+		
+		System.out.println("Conexão recebida de " + clienteSocket.getInetAddress());
+		
+		//cria duas threads para enviar e receber do cliente
+		ThreadRecebeCliente receber = new ThreadRecebeCliente(clienteSocket);
+		Thread thread = new Thread(receber);
+		thread.start();
+		
+		ThreadEnviaCliente send = new ThreadEnviaCliente(clienteSocket);
+		Thread thread2 = new Thread(send);
+		thread2.start();
+		//ss.close();
+	}
+
+}
+
+class ThreadRecebeCliente implements Runnable{
+	Socket clienteSocket = null;
+	BufferedReader buffReader = null;
+	
+	//construtor
+	public ThreadRecebeCliente(Socket clienteSocket){
+		this.clienteSocket = clienteSocket;
+	}
+	
+	
+	public void run() {
+		try{
+			buffReader = new BufferedReader(new InputStreamReader(this.clienteSocket.getInputStream()));		
+		
+			String mensagem;
+				while(true){
+					//atribuindo mensagem do cliente para variável mensagem
+					while((mensagem = buffReader.readLine())!= null){
+						if(mensagem.equals("exit")){
+							break;//fecha o socket se exit
+						}
+						System.out.println("do Cliente: " + mensagem);//mostra a mensagem do cliente
+					}
+				this.clienteSocket.close();
+				System.exit(0);
+				}		
+			} catch(Exception ex){System.out.println(ex.getMessage());}
+	}
+} // fim da classe ThreadRecebeCliente
+
+
+class ThreadEnviaCliente implements Runnable{
+	PrintWriter pwPrintWriter;
+	Socket clienteSocket = null;
+	
+	//construtor
+	public ThreadEnviaCliente(Socket clienteSocket){
+		this.clienteSocket = clienteSocket;
+	}
+	
+	//método da clase runnable
+	public void run() {
+		try{
+		pwPrintWriter =new PrintWriter(new OutputStreamWriter(this.clienteSocket.getOutputStream()));
+			while(true){
+				String mensagemParaCliente = null;
+				BufferedReader input = new BufferedReader(new InputStreamReader(System.in));//pegar entrada do usuário
+				
+				mensagemParaCliente = input.readLine();// pegar a mensagem para enviar pro cliente
+				
+				pwPrintWriter.println(mensagemParaCliente);//envia a mensagem pro Cliente com printWriter
+				pwPrintWriter.flush();//descarrega o PrintWriter
+			}
+		} catch(Exception ex){System.out.println(ex.getMessage());}	
+	}
+}
+
