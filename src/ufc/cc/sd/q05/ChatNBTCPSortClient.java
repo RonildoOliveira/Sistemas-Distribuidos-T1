3e0ed3a9ecf932045e05b7b8247d18986ea77435
RonlidoOliveira <ronildo.comp@gmail.com>
2016-10-21 05:41:11 -0200

    Questao 05 (Adiciona servico de ordenação de dos caracteres da mensagem)
    Signed-off-by: RonlidoOliveira <ronildo.comp@gmail.com>

---
+++ src/ufc/cc/sd/q05/ChatNBTCPSortClient.java
@@ -0,0 +1,34 @@
+package ufc.cc.sd.q05;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SocketChannel;
+import java.util.Scanner;
+ 
+public class ChatNBTCPSortClient {
+ 
+	@SuppressWarnings("resource")
+	public static void main(String[] args) throws IOException, InterruptedException {
+ 
+		InetSocketAddress inetSocketAddress = new InetSocketAddress("localhost", 1111);
+		SocketChannel socketChanel = SocketChannel.open(inetSocketAddress);
+ 
+		System.out.println("Conectando ao Servidor...");
+ 
+		String msg = null;
+		while (msg != "#") {
+			
+			msg = new Scanner(System.in).nextLine();
+			
+			byte[] message = new String(msg).getBytes();
+			ByteBuffer buffer = ByteBuffer.wrap(message);
+			socketChanel.write(buffer);
+ 
+			System.out.println("Enviado: [" + msg + "]");
+			buffer.clear();
+
+		}
+		socketChanel.close();
+	}
+}
---
+++ src/ufc/cc/sd/q05/ChatNBTCPSortServer.java
@@ -0,0 +1,84 @@
+package ufc.cc.sd.q05;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.Set;
+ 
+public class ChatNBTCPSortServer {
+ 
+	@SuppressWarnings("unused")
+	public static void main(String[] args) throws IOException {
+ 
+		// Multiplecador de of SelectableChannel objects
+		Selector selector = Selector.open(); // selector is open here
+ 
+		// ServerSocketChannel: selectable channel for stream-oriented listening sockets
+		ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+		InetSocketAddress inetSocketAddress = new InetSocketAddress("localhost", 1111);
+ 
+		// Binds the channel's socket to a local address and configures the socket to listen for connections
+		serverSocketChannel.bind(inetSocketAddress); 
+		serverSocketChannel.configureBlocking(false);
+ 
+		int ops = serverSocketChannel.validOps();
+		SelectionKey selectionKey = serverSocketChannel.register(selector, ops, null);
+ 
+		while (true) {
+ 
+			System.out.println("Servidor ativo...");
+			// Selects a set of keys whose corresponding channels are ready for I/O operations
+			selector.select();
+ 
+			// token representing the registration of a SelectableChannel with a Selector
+			Set<SelectionKey> crunchifyKeys = selector.selectedKeys();
+			Iterator<SelectionKey> crunchifyIterator = crunchifyKeys.iterator();
+ 
+			while (crunchifyIterator.hasNext()) {
+				SelectionKey myKey = crunchifyIterator.next();
+ 
+				// Tests whether this key's channel is ready to accept a new socket connection
+				if (myKey.isAcceptable()) {
+					
+					SocketChannel socketChannelClient = serverSocketChannel.accept();
+					socketChannelClient.configureBlocking(false);
+ 
+					// Operation-set bit for read operations
+					socketChannelClient.register(selector, SelectionKey.OP_READ);
+					System.out.println("Connection Accepted: " + socketChannelClient.getLocalAddress() + "\n");
+
+				} else if (myKey.isReadable()) {
+					
+					SocketChannel socketChannelClient = (SocketChannel) myKey.channel();
+					ByteBuffer byteBuffer = ByteBuffer.allocate(256);
+					socketChannelClient.read(byteBuffer);
+					String result = new String(byteBuffer.array()).trim();
+ 
+					if(result.charAt(0)=='_'){
+						
+				        char[] chars = result.toCharArray();
+				        Arrays.sort(chars);
+				        String sorted = new String(chars);
+				        System.out.println("Recebido: [" + sorted + "]");
+					}else{
+						System.out.println("Recebido: [" + result + "]");	
+					}
+					 
+					//condicao de parada (cliente envia #)
+					if (result.equals("#")) {
+						socketChannelClient.close();
+						System.out.println("Conexao parada");
+					}
+				}
+				crunchifyIterator.remove();
+			}
+		}
+	}
+ 
+}
